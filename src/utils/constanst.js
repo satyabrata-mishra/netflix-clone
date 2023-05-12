cebfdee6d2d80438d2b0c5b05826bf5c4fa1ce7d
Satyabrata Mishra <mishrasatyabrata27@gmail.com>
2023-05-12 17:31:40 +0530

    Changed the backend link

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,3 +1,3 @@
 export const api_key = "bf9f018b11ceb751c507a6351098fb02";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const host = "https://netflix-clone000.herokuapp.com";
+export const host = "https://netflix-clone-1pu9.onrender.com";
